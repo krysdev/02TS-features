1ecc8d8b4b94d8fe773f4104810267df8c11b45c
KrysDev <webdevkrys@gmail.com>
2023-04-26 21:31:24 +0100

    x

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -7,3 +7,30 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let vehicle: Car = new Car(); // vehicle is an instance of the Car class (v : C = )
+
+// Object literal
+let point: { x: number; y: number } = {
+  // semicolon in the annotation { x ; y }
+  // comas in the object
+  x: 10,
+  y: 20,
+};
+
+// Function
+
+// const logNumber = (i) => {
+//   console.log(i);
+// };
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
