0ff0d9397e1328df37fec3d76214d47f7b014dc3
KrysDev <webdevkrys@gmail.com>
2023-04-27 18:45:39 +0100

    x

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -26,11 +26,42 @@ let point: { x: number; y: number } = {
 };
 
 // Function
-
-// const logNumber = (i) => {
-//   console.log(i);
-// };
-
+//   const logNumber = (i) => { console.log(i) };
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// Annotations to be used when:
+
+// 1) a function returns the 'any' type
+
+const jsonstring = '{"x": 10, "y": 20}';
+// const coordinates = JSON.parse(jsonstring);
+const coordinates: { x: number; y: number } = JSON.parse(jsonstring);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) a variable is declared on one line and initalized later
+
+let words = ['red', 'green', 'blue'];
+// let foundWord; // declaration
+let foundWord: boolean; // declaration
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true; // initialization
+  }
+}
+
+// 3) the type of a variable cannot be inferred correctly
+
+// (the LOGIC: if a number is more than 0, then assign it, if not assign 'false')
+
+let numbers = [-10, -1, 12];
+// let numberAboveZero = false;
+let numberAboveZero: boolean | number = false; // initialized as boolean but numbers can be assign too
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
