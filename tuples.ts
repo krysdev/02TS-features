005853fef9b4608a4bd43c90856d2e68578f935f
KrysDev <webdevkrys@gmail.com>
2023-04-30 17:43:26 +0100

    x

---
+++ tuples.ts
@@ -0,0 +1,17 @@
+// this object is represented as a tuple below
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+// the elements in the below array can be swapped and the data model breaks down
+const pepsi: (string | number | boolean)[] = ['brown', true, 40];
+
+// so the tuple is an array-like that will always have a consistent order of the elements inside of it
+const sprite: [string, boolean, number] = ['clear', true, 40];
+sprite[0] = 10; // it can't be a number now
+
+// 'Drink' is a type alias
+type Drink = [string, boolean, number];
+const tea: Drink = ['brown', false, 0];
