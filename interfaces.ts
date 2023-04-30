a7a2abbc28cbee838970db450f37d5bd31868cca
KrysDev <webdevkrys@gmail.com>
2023-04-30 20:20:35 +0100

    x

--- interfaces.ts
+++ interfaces.ts
@@ -1,8 +1,51 @@
-interface Reportable {
-  summary(): string;
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true,
+};
+
+interface Vehicle {
+  name: string;
+  year: number;
+  broken: boolean;
 }
 
-const oldCivic = {
+// so the function's argument must be an object that meets the specification provided by the interface 'Vehicle'
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken: ${vehicle.broken}`);
+};
+
+printVehicle(oldCivic);
+
+//
+//
+/////////////// a type can be a complex object (Date)
+const oldCivic2 = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+};
+
+interface Vehicle2 {
+  name: string;
+  year: Date;
+  broken: boolean;
+}
+
+const printVehicle2 = (vehicle: Vehicle2): void => {
+  console.log(`Name: ${vehicle.name}`);
+  console.log(`Year: ${vehicle.year}`);
+  console.log(`Broken: ${vehicle.broken}`);
+};
+
+printVehicle2(oldCivic2);
+
+//
+//
+////////////// and that also includes functions
+const oldCivic3 = {
   name: 'civic',
   year: new Date(),
   broken: true,
@@ -10,6 +53,27 @@ const oldCivic = {
     return `Name: ${this.name}`;
   },
 };
+
+interface Vehicle3 {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string; // it has to be a function with no arguments that returns a string
+}
+
+const printVehicle3 = (vehicle: Vehicle3): void => {
+  console.log(vehicle.summary());
+};
+
+printVehicle3(oldCivic3);
+
+//
+//
+////////////////
+
+interface Reportable {
+  summary(): string;
+}
 
 const drink = {
   color: 'brown',
